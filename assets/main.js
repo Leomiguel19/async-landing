6481a839e8885aa4f3c205190c0ddf0185260177
Leomiguel19 <leomiguel1907@gmail.com>
2022-10-29 17:15:36 -0400

    Added new features like GOT API connection and dynamism.

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,10 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+const API = 'https://game-of-thrones1.p.rapidapi.com/Characters';
 const content = null || document.getElementById('content');
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-        'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+		'X-RapidAPI-Key': 'de94ba74ffmshd890529e2d76d71p19012bjsn63e67ca015a6',
+		'X-RapidAPI-Host': 'game-of-thrones1.p.rapidapi.com'
     }
 };
 async function fetchData(urlApi) {
@@ -15,22 +15,30 @@ async function fetchData(urlApi) {
 
 (async () => {
     try {
-        const videos = await fetchData(API);
+        const gots = await fetchData(API);
+
+        console.log(gots)
+        console.log(gots)
         let view = `
-            ${videos.items.map(video=>`
-                <div class="group r elative">
+            ${gots.map(got=>`
+                <div class="group relative">
                 <div
                     class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+                    <img src="${got.imageUrl}" alt="${got.title}" class="w-full">
                 </div>
                 <div class="mt-4 flex justify-between">
                     <h3 class="text-sm text-gray-700">
                     <span aria-hidden="true" class="absolute inset-0"></span>
-                    ${video.snippet.title}
+                    ${got.fullName}
+                    ${got.title}
                     </h3>
                 </div>
                 </div>
-            `).slice(0,4).join('')}
+            `).slice(0,20).join('')}
             `;
-    } catch {}
+        console.log(view);
+        content.innerHTML = view
+    } catch (error){
+        console.log(error);
+    }
 })();
